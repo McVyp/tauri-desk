f8993d34c0877c31b6ef222eacad8014534e7059
McVyp <lyeclyde@gmail.com>
2024-10-16 18:49:50 +0900

    add hours

--- app/components/timer.tsx
+++ app/components/timer.tsx
@@ -4,21 +4,28 @@ const format: ComponentProps<typeof MotionNumber>["format"] = {
   minimumIntegerDigits: 2,
 };
 export function Timer() {
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
 
-    useEffect(() =>{
-        const interval = setInterval(() =>{
-            setSeconds((prev)=> prev + 1);
-            if( seconds === 59) {
-                setSeconds(0);
-                setMinutes((prev) => prev + 1);
-            }
-        }, 100)
-        return () => clearInterval(interval);
-    },[seconds])
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+      if (seconds === 59) {
+        setSeconds(0);
+        setMinutes((prev) => prev + 1);
+      }
+      if (seconds === 59 && minutes === 59) {
+        setMinutes(0);
+        setHours((prev) => prev + 1);
+      }
+    }, 100);
+    return () => clearInterval(interval);
+  }, [minutes, seconds]);
   return (
     <div className="bg-gray-400 font-mono flex text-3xl">
+      <MotionNumber value={hours} format={format} />
+      <div>:</div>
       <MotionNumber value={minutes} format={format} />
       <div>:</div>
       <MotionNumber value={seconds} format={format} />
